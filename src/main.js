e747d587f676cb746e934088c883b2bcc294e686
Nika-yatashvili <nika.katashvili2@gmail.com>
2025-01-31 02:20:12 -0800

    assignment2

--- src/main.js
+++ src/main.js
@@ -1,40 +1,40 @@
 // Work in this directory: src
 // task 1
-let four = "number"
+let four = "number";
 
-console.log(four)
+console.log(four);
 
 // task2
-let five = "5"
-console.log(five + "is a string" )
+let five = "5";
+console.log(five + "is a string" );
 
-let z = 5
+let z = 5;
 
-console.log(z + "is a number")
+console.log(z + "is a number");
 
 // task 3 
 
-let money = 10 
-console.log(money)
+let money = 10 ;
+console.log(money);
 
 // task4
-let income = 100
+let income = 100;
 
-let bills = 30
+let bills = 30;
 
-console.log(income - bills)
+console.log(income - bills);
 
 // task5
 
 
 
 // task6
-let hello = 'Hello'
-console.log(hello.toUpperCase())
+let hello = 'Hello';
+console.log(hello.toUpperCase());
 
 // task7
-let earth = 'World'
-console.log(earth.toLowerCase())
+let earth = 'World';
+console.log(earth.toLowerCase());
 
 
 // task8
@@ -42,7 +42,7 @@ let text1 = 'Hello';
 text1.toUpperCase();
 console.log(`${text1} is unchanged`);
 
-console.log("task 9: " + " " + "text is undefined")
+console.log("task 9: " + " " + "text is undefined");
 
 // task10 
 let text = 'Hello';
@@ -56,11 +56,11 @@ console.log(`Next year you will be ${nextAge}`);
 
 // task13
 
-console.log("task 13:"+ " "+  "the value of x is unchanged")
+console.log("task 13:"+ " "+  "the value of x is unchanged");
 
 // task14 
 
-console.log("task 14:" + " " + "the value of y is 6")
+console.log("task 14:" + " " + "the value of y is 6");
 
 
 
