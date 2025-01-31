019d9b64d3fd3bc8b8f1c75dfce69a9df2605a70
Nika-yatashvili <nika.katashvili2@gmail.com>
2025-01-31 02:18:38 -0800

    assignment2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,66 @@
 // Work in this directory: src
+// task 1
+let four = "number"
+
+console.log(four)
+
+// task2
+let five = "5"
+console.log(five + "is a string" )
+
+let z = 5
+
+console.log(z + "is a number")
+
+// task 3 
+
+let money = 10 
+console.log(money)
+
+// task4
+let income = 100
+
+let bills = 30
+
+console.log(income - bills)
+
+// task5
+
+
+
+// task6
+let hello = 'Hello'
+console.log(hello.toUpperCase())
+
+// task7
+let earth = 'World'
+console.log(earth.toLowerCase())
+
+
+// task8
+let text1 = 'Hello';
+text1.toUpperCase();
+console.log(`${text1} is unchanged`);
+
+console.log("task 9: " + " " + "text is undefined")
+
+// task10 
+let text = 'Hello';
+console.log(text.toLowerCase());
+
+// task11 
+
+let age = prompt('How old are you?');
+let nextAge = ++age;
+console.log(`Next year you will be ${nextAge}`);
+
+// task13
+
+console.log("task 13:"+ " "+  "the value of x is unchanged")
+
+// task14 
+
+console.log("task 14:" + " " + "the value of y is 6")
+
+
+
